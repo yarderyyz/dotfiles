2a25f9ea47d4279745081bfa5c5990a0dda7d272
Lee Gauthier <lee.d.gauthier@gmail.com>
2024-05-21 14:57:38 +0200

    update nvim config

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -6,7 +6,9 @@ return {
       -- Conform will run multiple formatters sequentially
       python = { "isort", "black" },
       -- Use a sub-list to run only the first available formatter
-      javascript = { { "prettierd", "prettier" } },
+      javascript = { "biome" },
+      typescript = { "biome" },
+      typescriptreact = { "biome" },
     },
   },
 }
--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -12,9 +12,18 @@ return {
     local mason_lspconfig = require("mason-lspconfig")
     mason_lspconfig.setup({
       ensure_installed = { "pyright" },
+      ensure_installed = { "black" },
+      ensure_installed = { "biome" },
+      ensure_installed = { "tsserver" },
     })
     require("lspconfig").pyright.setup({
       capabilities = capabilities,
     })
+    require("lspconfig").biome.setup({
+      capabilities = capabilities,
+    })
+    -- require("lspconfig").tsserver.setup({
+    --   capabilities = capabilities,
+    -- })
   end,
 }
